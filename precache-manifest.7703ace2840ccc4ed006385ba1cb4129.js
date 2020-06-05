506df15dd05bb360d76f65707bd6ea5c117ea31b
Aaron Garner <aaron.garner.1982@gmail.com>
2020-06-04 19:34:40 -0700

    Updates

---
+++ precache-manifest.7703ace2840ccc4ed006385ba1cb4129.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e878009413708f40e85ed4dda4bb158f",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "3979be519b6b775ce165",
+    "url": "/memory-game/static/css/main.edfe0b6c.chunk.css"
+  },
+  {
+    "revision": "8d9cf1cf6c3fc8dc5619",
+    "url": "/memory-game/static/js/2.d7921de6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.d7921de6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3979be519b6b775ce165",
+    "url": "/memory-game/static/js/main.b1b4d0c0.chunk.js"
+  },
+  {
+    "revision": "885fc3b766c7910c8699",
+    "url": "/memory-game/static/js/runtime-main.dc19f532.js"
+  }
+]);
