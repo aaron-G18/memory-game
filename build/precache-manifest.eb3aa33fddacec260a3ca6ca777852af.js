d013514159e80a3e8fb7f3d365467159bda33e0f
Aaron Garner <aaron.garner.1982@gmail.com>
2020-06-06 12:15:44 -0700

    pushing up after running deploy

--- build/precache-manifest.eb3aa33fddacec260a3ca6ca777852af.js
+++ build/precache-manifest.eb3aa33fddacec260a3ca6ca777852af.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f3906b6385b0f79eeec68c1446c07d66",
+    "revision": "da97704b14e69d015362f127828978a6",
     "url": "/memory-game/index.html"
   },
   {
-    "revision": "3fb043f1fdd929abd1a3",
+    "revision": "e08a91e6ae7cc4d4839a",
     "url": "/memory-game/static/css/main.edfe0b6c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/memory-game/static/js/2.d7921de6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3fb043f1fdd929abd1a3",
-    "url": "/memory-game/static/js/main.dec6981a.chunk.js"
+    "revision": "e08a91e6ae7cc4d4839a",
+    "url": "/memory-game/static/js/main.566ed789.chunk.js"
   },
   {
     "revision": "885fc3b766c7910c8699",
