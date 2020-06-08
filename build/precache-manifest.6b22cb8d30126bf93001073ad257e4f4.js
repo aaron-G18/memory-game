bea20f2e55c09a137223bc8c3c572fbd73b52006
Aaron Garner <aaron.garner.1982@gmail.com>
2020-06-08 09:06:01 -0700

    updated the name in package.json

---
+++ build/precache-manifest.6b22cb8d30126bf93001073ad257e4f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fa4a3b50f89d559b4d783a0e6f08a64",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "a9ed17eb473f14fa0ad9",
+    "url": "/memory-game/static/css/main.54cc5916.chunk.css"
+  },
+  {
+    "revision": "17f0d5800f81a6cff98f",
+    "url": "/memory-game/static/js/2.e9f12438.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.e9f12438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9ed17eb473f14fa0ad9",
+    "url": "/memory-game/static/js/main.9a6d361d.chunk.js"
+  },
+  {
+    "revision": "7efe6da8554f191a92ed",
+    "url": "/memory-game/static/js/runtime-main.41f480ba.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/memory-game/precache-manifest.7ffc3d4074e7998c188a1232fe82316d.js"
+  "/memory-game/precache-manifest.6b22cb8d30126bf93001073ad257e4f4.js"
 );
 
 self.addEventListener('message', (event) => {
